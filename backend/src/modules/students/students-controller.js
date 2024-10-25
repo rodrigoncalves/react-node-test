72365fa37a3e1774cf093c76f63c6326965a5346
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-10-25 01:12:14 -0300

    Implement student controller methods

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,31 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const students = await getAllStudents(req.query);
+    res.json({ students })
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const userId = req.params.id;
+    const payload = req.body;
+    const message = await updateStudent({ ...payload, userId });
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const student = await getStudentDetail(req.params.id);
+    res.json({ student })
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const message = await setStudentStatus(req.params.id, req.body.status);
+    res.json(message);
 });
 
 module.exports = {
